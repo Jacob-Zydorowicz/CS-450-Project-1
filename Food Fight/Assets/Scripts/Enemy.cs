d1ecaa5f1685da4320a0d7f051476074567630a6
King-Julian11 <blazerman741@gmail.com>
2023-02-03 13:28:32 -0600

    Enemy Script Start

---
+++ Food Fight/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+
+    public float health;
+    public float damage;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
