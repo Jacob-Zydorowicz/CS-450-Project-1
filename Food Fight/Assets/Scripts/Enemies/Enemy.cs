2c0e27949ac1b771e4daed6a0c63587becdade64
King-Julian11 <blazerman741@gmail.com>
2023-02-14 20:54:20 -0600

    Enemy Movement

--- Food Fight/Assets/Scripts/Enemies/Enemy.cs
+++ Food Fight/Assets/Scripts/Enemies/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
 
     public float health;
@@ -16,7 +16,9 @@ public abstract class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 10;
+        damage = 10;
+        speed = 1;
     }
 
     // Update is called once per frame
@@ -27,7 +29,10 @@ public abstract class Enemy : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        if (distance < 4)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
     }
 }
