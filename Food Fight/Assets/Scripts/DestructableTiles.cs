653ae76ebee922fffd47162f11432f3df28ab22e
Jacob-Zydorowicz <96926683+Jacob-Zydorowicz@users.noreply.github.com>
2023-02-18 21:48:08 -0600

    2nd Level Layout

--- Food Fight/Assets/Scripts/DestructableTiles.cs
+++ Food Fight/Assets/Scripts/DestructableTiles.cs
@@ -15,20 +15,29 @@ public class DestructableTiles : MonoBehaviour
 
     private void Start()
     {
-        destructableTiles = GetComponent<Tilemap>();
+        destructableTiles = GameObject.FindGameObjectWithTag("destructible").GetComponent<Tilemap>();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    /*   private void OnTriggerEnter2D(Collider2D collision)
+       {
+           if(collision.gameObject.CompareTag("destructible"))
+           {
+
+               Destroy(gameObject);
+               Destroy(Collision.)
+           }
+       }*/
+    private void OnTriggerEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Projectiles"))
+        if (collision.gameObject.CompareTag("destructible"))
         {
             Debug.Log("hit");
             Vector3 hitPos = Vector3.zero;
 
-            foreach(ContactPoint2D hit in collision.contacts)
+            foreach (ContactPoint2D hit in collision.contacts)
             {
                 hitPos.x = hit.point.x - 0.01f * hit.normal.x;
-                hitPos.y= hit.point.y - 0.01f * hit.normal.y;
+                hitPos.y = hit.point.y - 0.01f * hit.normal.y;
 
                 destructableTiles.SetTile(destructableTiles.WorldToCell(hitPos), null);
             }
