ec1ada158fe3320980154145193a2b27b6921a7c
Jacob-Zydorowicz <96926683+Jacob-Zydorowicz@users.noreply.github.com>
2023-02-21 00:15:47 -0600

    Added basic pause menu

---
+++ Food Fight/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,51 @@
+/*
+ * Jacob Zydorowicz
+ * PauseGame.cs
+ * Food Fight
+ * Basic pause menu script
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public static bool gameIsPaused = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+                ResumeTheGame();
+            else
+                PauseTheGame();
+        }
+    }
+
+    public void ResumeTheGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        gameIsPaused = false;
+    }
+
+    private void PauseTheGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
